e49e70f14bceb75fa3b656c363fdd87a0b3babba
Helena Nel <mailhedri@gmail.com>
2021-05-19 17:24:38 +0200

    commit

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 function changeViewToHideButton(par) {
-  const selectViewButtons = document.querySelector("#hide-button");
-  selectViewButtons.innerHTML = "hide";
+  const selectViewButtons = document.querySelectorAll(
+    "div.btn-group>button:nth-child(2)"
+  );
+  selectViewButtons.forEach((btn) => (btn.innerHTML = "Hide"));
+  selectViewButtons.forEach((btn) => btn.setAttribute("id", "hide-button"));
+  selectViewButtons.forEach((btn) => btn.setAttribute("data-toggle", "modal"));
+  //   for (let i = 0; i < selectViewButtons.length; i++) {
+  //     selectViewButtons[i].innerHTML = "hide";
+  //   }
 }
 
-// 1) When pressing on Load Images button, load the pictures from http://www.splashbase.co/api/v1/images/search?query=your query
+function myModal(event) {
+  console.log(event.target); //what we clicked on
+  const theModal = document.querySelector(".modal-body");
+  let images = document.querySelectorAll(".card>.card-img");
+  images.forEach((img) => {
+    if (img.id === event.target.id) {
+      //the id of the image is the same id we gave to the button
+      theModal.innerHTML += `<img src=${img.url}}>`;
+    }
+  });
+  const selected_img = images.filter((im) => im.id === id);
+  console.log(selected_img);
+}
 
 // const loadButton = document.querySelector(.)
 function loadImages({ images }) {
@@ -16,6 +35,14 @@ function loadImages({ images }) {
     img.className = "card-img";
     img.src = images[i].url;
     card.innerHTML = img.outerHTML + " " + card.innerHTML;
+
+    let view_btns = document.querySelectorAll(
+      "div.btn-group>button:nth-child(1)"
+    );
+    view_btns.forEach((btn) => {
+      btn.addEventListener("click", (event) => myModal(event));
+      btn.id = images[i].id;
+    });
   }
 }
 
@@ -33,3 +60,6 @@ window.onload = () => changeViewToHideButton("Hide");
 //         myModal.innerHTML= `<img class="modal-image" src=${images.url}>`
 //     }
 // }
+
+const inputField = document.querySelector("input").value;
+const searchFunc = () => getData().filter(inputField);
